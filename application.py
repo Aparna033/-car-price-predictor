8624b694317f07e256798222b47795a4512d7471
Aparna033 <bgsaparnakumari@gmail.com>
2026-06-05 17:26:23 +0530

    Updated car price predictor UI

--- application.py
+++ application.py
@@ -47,25 +47,14 @@ def index():
 @app.route('/predict', methods=['POST'])
 @cross_origin()
 def predict():
-    # 1. Numeric Inputs (Properly Casted)
-    car_ID = int(request.form.get('car_ID'))
-    symboling = int(request.form.get('symboling'))
-    wheelbase = float(request.form.get('wheelbase'))
-    carlength = float(request.form.get('carlength'))
-    carwidth = float(request.form.get('carwidth'))
-    carheight = float(request.form.get('carheight'))
-    curbweight = int(request.form.get('curbweight'))
-    enginesize = int(request.form.get('enginesize'))
-    boreratio = float(request.form.get('boreratio'))
-    stroke = float(request.form.get('stroke'))
-    compressionratio = float(request.form.get('compressionratio'))
-    horsepower = int(request.form.get('horsepower'))
-    peakrpm = int(request.form.get('peakrpm'))
-    citympg = int(request.form.get('citympg'))
-    highwaympg = int(request.form.get('highwaympg'))
 
-    # 2. Categorical Inputs
+     # 2. Categorical Inputs
     CarName = request.form.get('CarName')
+
+    if CarName == "Select Car Name":
+        return "Please select a valid car"
+    selected_car = car[car['CarName'] == CarName].iloc[0]
+
     fueltype = request.form.get('fueltype')
     aspiration = request.form.get('aspiration')
     doornumber = request.form.get('doornumber')
@@ -75,7 +64,31 @@ def predict():
     enginetype = request.form.get('enginetype')
     cylindernumber = request.form.get('cylindernumber')
     fuelsystem = request.form.get('fuelsystem')
+ 
+    # 1. Numeric Inputs (Properly Casted)
+    car_ID = int(selected_car['car_ID'])
+    symboling = int(selected_car['symboling'])
 
+    wheelbase = float(selected_car['wheelbase'])
+    carlength = float(selected_car['carlength'])
+    carwidth = float(selected_car['carwidth'])
+    carheight = float(selected_car['carheight'])
+
+    curbweight = int(selected_car['curbweight'])
+
+    enginesize = int(selected_car['enginesize'])
+
+    boreratio = float(selected_car['boreratio'])
+    stroke = float(selected_car['stroke'])
+
+    compressionratio = float(selected_car['compressionratio'])
+
+    horsepower = int(selected_car['horsepower'])
+
+    peakrpm = int(selected_car['peakrpm'])
+
+    citympg = int(selected_car['citympg'])
+    highwaympg = int(selected_car['highwaympg'])
     # 3. Create the DataFrame using a standard Python list of lists [[ ... ]]
     # This prevents pandas/numpy from forcing your numbers into strings!
     input_df = pd.DataFrame(
@@ -98,10 +111,13 @@ def predict():
     )
 
     # 4. Predict
-    prediction = model.predict(input_df)
-    print(prediction)
+    prediction = model.predict(input_df)[0]
+    print("Prediction:", prediction)
 
-    return str(np.round(prediction[0], 2))
+    if prediction < 0:
+        return "Cannot predict the price"
+    
+    return str(round(prediction, 2))
 
 
 if __name__ == "__main__":
